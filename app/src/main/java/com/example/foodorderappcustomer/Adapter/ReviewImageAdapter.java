03ea8d2c1b906a69b51a0593d220b05b16a5e972
hlchi13 <chihoang.uet@gmail.com>
2025-05-25 14:26:18 +0700

    add dish rating feature

---
+++ app/src/main/java/com/example/foodorderappcustomer/Adapter/ReviewImageAdapter.java
@@ -0,0 +1,87 @@
+package com.example.foodorderappcustomer.Adapter;
+
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ImageButton;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.foodorderappcustomer.R;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReviewImageAdapter extends RecyclerView.Adapter<ReviewImageAdapter.ImageViewHolder> {
+    private List<Uri> images;
+    private OnImageRemoveListener listener;
+
+    public interface OnImageRemoveListener {
+        void onImageRemove(int position);
+    }
+
+    public ReviewImageAdapter(OnImageRemoveListener listener) {
+        this.images = new ArrayList<>();
+        this.listener = listener;
+    }
+
+    @NonNull
+    @Override
+    public ImageViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_review_image, parent, false);
+        return new ImageViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ImageViewHolder holder, int position) {
+        Uri imageUri = images.get(position);
+        Picasso.get()
+                .load(imageUri)
+                .fit()
+                .centerCrop()
+                .into(holder.imageView);
+
+        holder.removeButton.setOnClickListener(v -> {
+            if (listener != null) {
+                listener.onImageRemove(position);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return images.size();
+    }
+
+    public void addImage(Uri imageUri) {
+        images.add(imageUri);
+        notifyItemInserted(images.size() - 1);
+    }
+
+    public void removeImage(int position) {
+        if (position >= 0 && position < images.size()) {
+            images.remove(position);
+            notifyItemRemoved(position);
+        }
+    }
+
+    public List<Uri> getImages() {
+        return images;
+    }
+
+    static class ImageViewHolder extends RecyclerView.ViewHolder {
+        ImageView imageView;
+        ImageButton removeButton;
+
+        public ImageViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.imageView);
+            removeButton = itemView.findViewById(R.id.removeButton);
+        }
+    }
+} 
---
+++ app/src/main/java/com/example/foodorderappcustomer/Models/Review.java
@@ -0,0 +1,74 @@
+package com.example.foodorderappcustomer.Models;
+
+import java.util.Date;
+import java.util.List;
+
+public class Review {
+    private String userId;
+    private String orderId;
+    private String restaurantId;
+    private float rating;
+    private String comment;
+    private Date timestamp;
+    private List<String> imageUrls;
+
+    public Review() {
+        // Required empty constructor for Firebase
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getRestaurantId() {
+        return restaurantId;
+    }
+
+    public void setRestaurantId(String restaurantId) {
+        this.restaurantId = restaurantId;
+    }
+
+    public float getRating() {
+        return rating;
+    }
+
+    public void setRating(float rating) {
+        this.rating = rating;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public List<String> getImageUrls() {
+        return imageUrls;
+    }
+
+    public void setImageUrls(List<String> imageUrls) {
+        this.imageUrls = imageUrls;
+    }
+} 
